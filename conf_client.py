7d9e1c1552b616e1bcbfd551dc650e982beb2b66
Pluviophil3 <12212225@mail.sustech.edu.cn>
2024-11-23 20:26:12 +0800

    test

--- conf_client.py
+++ conf_client.py
@@ -53,7 +53,7 @@ class ConferenceClient:
         pass
 
     def keep_recv(self, recv_conn, data_type, decompress=None):
-        #接收
+        #接收2
         '''
         running task: keep receiving certain type of data (save or output)
         you can create other functions for receiving various kinds of data
